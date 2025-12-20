9aaaa69b916ff40297b6306299ba71143515f8fd
pzy <1697437561@qq.com>
2025-12-20 08:19:25 +0000

    add hnsw metrics counter

--- ann_benchmarks/algorithms/hnswlib/module.py
+++ ann_benchmarks/algorithms/hnswlib/module.py
@@ -21,6 +21,7 @@ class HnswLib(BaseANN):
         self.p.set_num_threads(1)
 
     def set_query_arguments(self, ef):
+        self.p.reset_metrics_counter()
         self.p.set_ef(ef)
         self.name = "hnswlib (%s, 'efQuery': %s)" % (self.method_param, ef)
 
@@ -33,21 +34,14 @@ class HnswLib(BaseANN):
         """从C++ binding获取性能指标"""
         # 建议的C++ binding方法名称（需要在Python binding中实现）：
         # - get_cache_hit_rate(): 返回浮点数百分比
-        # - get_io_operations(): 返回整数I/O操作数
+        # - get_io_op_num(): 返回整数I/O操作数
         # - get_memory_transfer_kb(): 返回浮点数KB单位
 
         return {
             "cache_hit_rate": float(self.p.get_cache_hit_rate()),
-            "io_operations": int(self.p.get_io_operations()),
+            "io_operations": int(self.p.get_io_op_num()),
             "memory_transfer_kb": float(self.p.get_memory_transfer_kb())
         }
-
-    def done(self):
-        """清理资源，C++ binding需要调用清理方法"""
-        # 建议在C++ binding中实现：reset_counters()
-        if hasattr(self.p, 'reset_counters'):
-            self.p.reset_counters()
-        super().done()
 
     def freeIndex(self):
         del self.p
